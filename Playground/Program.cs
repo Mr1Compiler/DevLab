15c40c33725b2f370f4a9a3c5688f5fdcbea8efa
mr1compiler <mr1compiler@gmail.com>
2025-09-24 16:29:38 +0300

    New Project Structure

---
+++ Playground/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Udp.Server;
+using Udp.Client;
+
+
+Task.Run(() =>
+{
+    UdpServer udpServer = new UdpServer();
+    udpServer.Connect();
+    udpServer.Connect();
+});
+
+
+while (true)
+{
+    var msg = Console.ReadLine();
+    Client client = new Client();
+    client.Send(msg);
+}
