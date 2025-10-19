e2cfc39fc1b63366b10d7f673fd25ebb4da465bd
mr1compiler <mr1compiler@gmail.com>
2025-10-19 22:28:02 +0300

    Using the cors service and middleware by asp.net

--- src/ASP.NET/MyFirstApp/Program.cs
+++ src/ASP.NET/MyFirstApp/Program.cs
@@ -15,19 +15,19 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Custom middleware to accept cors requests with custom headers
-app.Use(async (ctx, next) =>
-{
-    ctx.Response.Headers["Access-Control-Allow-Origin"] = "http://localhost:5288";
-
-    if (HttpMethods.IsOptions(ctx.Request.Method))
-    {
-        ctx.Response.Headers["Access-Control-Allow-headers"] = "my-a, my-b";
-        await ctx.Response.CompleteAsync();
-    }
-
-    await next();
-});
+// ** Custom middleware to accept cors requests with custom headers **
+// app.Use(async (ctx, next) =>
+// {
+//     ctx.Response.Headers["Access-Control-Allow-Origin"] = "http://localhost:5288";
+//
+//     if (HttpMethods.IsOptions(ctx.Request.Method))
+//     {
+//         ctx.Response.Headers["Access-Control-Allow-headers"] = "my-a, my-b";
+//         await ctx.Response.CompleteAsync();
+//     }
+//
+//     await next();
+// });
 
 app.MapGet("/", () => "Hello Ayman");
 
